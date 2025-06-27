1380c26fddaff81737592dac3e9d026882e45b69
mltomacheski <ltomacheski.lara@escola.pr.gov.br>
2025-06-27 10:40:32 -0300

    sei lá

---
+++ src/main/java/com/tulipa/flor/FlorApplication.java
@@ -0,0 +1,13 @@
+package com.tulipa.flor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tulipa/flor/FlorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tulipa.flor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
