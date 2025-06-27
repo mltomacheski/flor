d01a2d11e878bd6f6e4909a4668097290e228817
mltomacheski <ltomacheski.lara@escola.pr.gov.br>
2025-06-27 12:19:19 -0300

    Primeira requisição feita

---
+++ src/main/java/com/tulipa/flor/controller/ProfessorController.java
@@ -0,0 +1,17 @@
+package com.tulipa.flor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping( value = "/professor")
+public class ProfessorController {
+    
+    @GetMapping( value = "/")
+    public void imprimir() {
+        System.out.println("Chamou o método imprimir");
+    }
+
+}
