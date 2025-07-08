5a5162a62c07aa77dc4802c2654ea3bf549de5fd
mltomacheski <ltomacheski.lara@escola.pr.gov.br>
2025-07-08 11:33:46 -0300

    implementação do select de um professor

--- src/main/java/com/tulipa/flor/controller/ProfessorController.java
+++ src/main/java/com/tulipa/flor/controller/ProfessorController.java
@@ -2,11 +2,14 @@ package com.tulipa.flor.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jersey.JerseyProperties.Servlet;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,12 +40,25 @@ public class ProfessorController {
     }
 
 
-    @GetMapping( value =  "/listaprofessor") 
+    @GetMapping( value =  "/listaprofessor") //Método de select do Bd => listar 
     public List<Professor> findAll(){
         return professorRepository.findAll();
-
     }
 
+    @DeleteMapping(value =  "/{id}") //Método de delete do Bd => excluir
+    public ResponseEntity<Void> delete( @PathVariable Long id){
+        professorRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping( value = "/{id}" )
+    public ResponseEntity <Professor> buscarprofessor(@PathVariable Long id) {
+        Optional<Professor> professorBanco =  professorRepository.findById(id);
+        return ResponseEntity.ok(professorBanco.get());
+    }
+
+    
     @PostMapping(value = "/insert")
     public ResponseEntity<?> insert(@RequestBody ProfessorDto professorDto) {
 
